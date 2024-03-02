0a212f9ae8ec3a00ca67d0fa89bebca197642b77
Cedric Lapi <lapicedric@gmail.com>
2024-03-02 14:52:53 +0100

    first commit

---
+++ contain.js
@@ -0,0 +1,65 @@
+class Node{
+
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    
+    constructor(){
+
+        this.head = null;
+    }
+
+
+    findSum(){
+
+        if(!this.head){
+            return 0;
+        }
+
+        var runner = this.head;
+        var sum = 0;
+
+
+        while(runner !== null){
+
+            sum += runner.data;
+            runner = runner.next;
+
+        }
+
+        return sum;
+
+    }
+
+
+    
+    contain(value){
+
+        var mover = this.head;
+
+        if(!this.head){
+
+            return false;
+        }
+
+        while(mover !== null){
+
+            if(mover.data === value ){
+
+                return true;
+              
+            }
+
+            mover = mover.next;
+        }
+
+        return false;
+
+
+    }
+
+}
